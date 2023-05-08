b781cc126a0f85881a766c32bdb9dbb2c9409f83
resinbeard <mrafoster@gmail.com>
2023-05-07 19:16:28 -0700

    icons will now transition properly between variations on refresh, initial icon weight is now correct

--- src/litegraph-editor.js
+++ src/litegraph-editor.js
@@ -114,6 +114,8 @@ Editor.prototype.createSpan = function(name, materials_icon_name, callback) {
     button.classList.add("btn-primary");    
     button.classList.add("material-symbols-outlined");
     button.classList.add("icons");
+    button.style.setProperty('--variation', `'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 48`);
+    
     button.setAttribute('title', name);
     // button.innerHTML += name;
     if(callback)
@@ -171,9 +173,9 @@ Editor.prototype.onLiveButton = function() {
     var button = this.root.querySelector("#livemode_button");
 
     if (is_live_mode) {
-        button.style.setProperty('--variation', `'FILL' 1, 'wght' 700, 'GRAD' 0, 'opsz' 48`);
+        button.style.setProperty('--variation', `'FILL' 1, 'wght' 400, 'GRAD' 0, 'opsz' 48`);
     } else {
-        button.style.setProperty('--variation', `'FILL' 0, 'wght' 700, 'GRAD' 0, 'opsz' 48`);
+        button.style.setProperty('--variation', `'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 48`);
     }
     
     // button.innerHTML = !is_live_mode
